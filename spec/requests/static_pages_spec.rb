5f5cc71f757d78564b471351eee7a047fabd466e
Ikuo Terado <eqobar@gmail.com>
2013-09-18 23:38:57 +0900

    Change title to have each page title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Petroglyph" }
+
   describe "Home page" do
 
     it "should have the content 'Petroglyph'" do
       visit '/static_pages/home'
       expect(page).to have_content('Petroglyph')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
     end
 
   end
@@ -18,14 +25,23 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-  end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
 
+  end
 
   describe "About page" do
 
     it "should have the content 'About" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About")
     end
 
   end
